d7da9ddbf0c8a6e18c4c84788ffe3a325a6e58c0
Gustavo <gustavo.ribeiro.fernandes1@gmail.com.br>
2023-07-01 18:15:15 -0300

    feat: :zap: adding back link to pureFunctionalComponent

--- src/components/pure-functional-component/PureFunctionalComponent.tsx
+++ src/components/pure-functional-component/PureFunctionalComponent.tsx
@@ -1,8 +1,10 @@
-import useCounter from './hooks/useCounter';
+import { Link } from 'react-router-dom';
 
 import { Counter } from './components/counter';
 import { DecrementButton } from './components/decrement-button';
 import { IncrementButton } from './components/increment-button';
+
+import useCounter from './hooks/useCounter';
 
 export default function PureFunctionalComponent() {
   const { count, handleIncrement, handleDecrement } = useCounter();
@@ -14,6 +16,8 @@ export default function PureFunctionalComponent() {
         increment={<IncrementButton handleClick={handleIncrement} />}
         decrement={<DecrementButton handleClick={handleDecrement} />}
       />
+
+      <Link to="/">Back</Link>
     </div>
   );
 }
